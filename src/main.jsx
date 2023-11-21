452ba195232bb3be36e36f777f8db305b97d53eb
JerryLin083 <126867794+JerryLin083@users.noreply.github.com>
2023-11-21 12:40:12 +0800

    remove react-error-boundary
    only use errorElement

--- src/main.jsx
+++ src/main.jsx
@@ -5,15 +5,11 @@ import { Provider } from "react-redux";
 import App from "./App.jsx";
 import store from "./store.js";
 import "./index.css";
-import { ErrorBoundary } from "react-error-boundary";
-import ErrorFallback from "./features/ui/ErrorFallback.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <ErrorBoundary FallbackComponent={ErrorFallback}>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </ErrorBoundary>
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
