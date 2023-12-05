93f1721d9931e9809f46091071b7f711430038ce
JerryLin083 <126867794+JerryLin083@users.noreply.github.com>
2023-12-05 20:34:34 +0800

    create .env variable

--- src/api/supabase.js
+++ src/api/supabase.js
@@ -1,8 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = "https://gvaxsgcsyzfqoxwnlrak.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd2YXhzZ2NzeXpmcW94d25scmFrIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTkyNzAxNjQsImV4cCI6MjAxNDg0NjE2NH0.JdC6hk5U-fAui2NfI5New_plqogKpYTSpo6ZJjRS2u8";
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
