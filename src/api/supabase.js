6c53cdfa11334705f86b565ffd375f4015cf35ae
JerryLin083 <126867794+JerryLin083@users.noreply.github.com>
2023-12-06 11:32:22 +0800

    Update supabase.js

--- src/api/supabase.js
+++ src/api/supabase.js
@@ -2,6 +2,7 @@ import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = "https://gvaxsgcsyzfqoxwnlrak.supabase.co";
 const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
+console.log(supabaseKey);
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
