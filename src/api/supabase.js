d82c01828a634994dd0a0d9da5ad1d233cbfb956
JerryLin083 <126867794+JerryLin083@users.noreply.github.com>
2023-12-06 10:38:50 +0800

    Update supabase.js

--- src/api/supabase.js
+++ src/api/supabase.js
@@ -2,6 +2,8 @@ import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = "https://gvaxsgcsyzfqoxwnlrak.supabase.co";
 const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
+console.log(supabaseKey);
+
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
